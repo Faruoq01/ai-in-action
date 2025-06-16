c679820bcd914c8c55a75536cfd6f925f79d3301
Faruoq01 <59730816+Faruoq01@users.noreply.github.com>
2025-06-16 05:12:32 +0100

    Update main.py

--- consultant-server/main.py
+++ consultant-server/main.py
@@ -1,8 +1,6 @@
-import subprocess
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
-from starlette.middleware.trustedhost import TrustedHostMiddleware
 from starlette.middleware.gzip import GZipMiddleware
 from controllers.user import router as climate_router
 from controllers.prompt import router as prompt_router
@@ -10,9 +8,6 @@ from controllers.prompt import router as prompt_router
 app = FastAPI()
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 app.add_middleware(HTTPSRedirectMiddleware)
-app.add_middleware(
-    TrustedHostMiddleware, allowed_hosts=["api.smart-consultant.online", "smart-consultant.online", "localhost", "127.0.0.1"]
-)
 
 # CORS - cross-origin security
 app.add_middleware(
@@ -31,12 +26,5 @@ app.include_router(prompt_router, prefix="/api/v1", tags=["Prompt"])
 async def root():
     return "Hello welcome to smart health consultant, login and try it out"
 
-@app.post("/deploy")
-async def webhook_handler(request: Request):
-    payload = await request.json()
-    print("Received webhook from main:", payload)
-
-    subprocess.Popen(["./deploy.sh"])
-    return {"status": "OK"}
 
 
