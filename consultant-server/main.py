116783c47ebdd417bc516ac096b88f7f0fa269f7
Faruoq01 <59730816+Faruoq01@users.noreply.github.com>
2025-06-16 07:49:25 +0100

    update

--- consultant-server/main.py
+++ consultant-server/main.py
@@ -1,8 +1,11 @@
+import asyncio
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.gzip import GZipMiddleware
 from controllers.user import router as climate_router
 from controllers.prompt import router as prompt_router
+from services.agent import AgentService
+agent = AgentService()
 
 app = FastAPI()
 app.add_middleware(GZipMiddleware, minimum_size=1000)
@@ -23,6 +26,10 @@ app.include_router(prompt_router, prefix="/api/v1", tags=["Prompt"])
 @app.get("/")
 async def root():
     return "Hello welcome to smart health consultant, login and try it out"
+
+@app.on_event("startup")
+async def start_background_task():
+    asyncio.create_task(agent.generate_all_embeddings())
 
 
 
